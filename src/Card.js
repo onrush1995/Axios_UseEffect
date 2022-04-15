7ef7c560d4d851255183388fd5b47051d3f2b99e
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-16 02:22:48 +0300

    Added style to the application

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,18 @@
 import React from "react";
+import "./App.css"
 import { Link } from "react-router-dom";
 
 
 const Card=({data})=>{
 return(<>
     {data.map((item,index)=>(<div key={index} className="card">
-      <h1>{item.name}</h1>
-      <p>{item.username}</p>
-      <Link to={`/cards/${item.name}`}>View more</Link>
-
+      <h1 className="circle">{item.name.charAt(0)}</h1>
+      <h3>{item.name}</h3>
+      <p>@{item.username}</p>
+      <a href={"http://{item.website"}>http://{item.website}</a>
+      <div>
+      <Link  style={{background:"#FA4616"}} to={`/cards/${item.name}`}>View more</Link>
+      </div>
     </div>))}
 </>)
 }
